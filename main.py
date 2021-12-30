4862638b6bf1274ee790b89d571789a3101e1767
sechevar <sechevar@ciena.com>
2021-12-30 00:14:38 -0300

    Debug data

--- main.py
+++ main.py
@@ -1,9 +1,8 @@
 # Author: Santiago Echevarria (santiago93echevarria@gmail.com)
-# 
 # Pull map from https://www.bing.com/maps/
 # Input: initial coordinates, length (km) to East and South
-# 
 # Source: https://docs.microsoft.com/en-us/bingmaps/articles/bing-maps-tile-system?redirectedfrom=MSDN
+
 
 import math
 from PIL import Image
@@ -14,7 +13,45 @@ import io
 import time
 import math
 
-LEVEL = 19
+
+# Level of Detail:	
+LEVEL = 19 
+
+# dim: Map Width and Height (pixels)	
+# res: Ground Resolution (meters / pixel)	
+# scale: Map Scale (at 96 dpi)  1:XX
+DATA = {
+    1:	{'dim':	512,	    'res':	78271.5170	,'scale':	 295829355.45},
+    2:	{'dim':	1024,	    'res':	39135.7585	,'scale':	 147914677.73},
+    3:	{'dim':	2048,	    'res':	19567.8792	,'scale':	 73957338.86},
+    4:	{'dim':	4096,	    'res':	9783.9396	,'scale':	 36978669.43},
+    5:	{'dim':	8192,	    'res':	4891.9698	,'scale':	 18489334.72},
+    6:	{'dim':	16384,	    'res':	2445.9849	,'scale':	 9244667.36},
+    7:	{'dim':	32768,	    'res':	1222.9925	,'scale':	 4622333.68},
+    8:	{'dim':	65536,	    'res':	611.4962	,'scale':	 2311166.84},
+    9:	{'dim':	131072,	    'res':	305.7481	,'scale':	 1155583.42},
+    10:	{'dim':	262144,	    'res':	152.8741	,'scale':	 577791.71},
+    11:	{'dim':	524288,	    'res':	76.4370	    ,'scale':	 288895.85},
+    12:	{'dim':	1048576,	'res':	38.2185	    ,'scale':	 144447.93},
+    13:	{'dim':	2097152,	'res':	19.1093	    ,'scale':	 72223.96},
+    14:	{'dim':	4194304,	'res':	9.5546	    ,'scale':	 36111.98},
+    15:	{'dim':	8388608,	'res':	4.7773	    ,'scale':	 18055.99},
+    16:	{'dim':	16777216,	'res':	2.3887	    ,'scale':	 9028.00},
+    17:	{'dim':	33554432,	'res':	1.1943	    ,'scale':	 4514.00},
+    18:	{'dim':	67108864,	'res':	0.5972	    ,'scale':	 2257.00},
+    19:	{'dim':	134217728,	'res':	0.2986	    ,'scale':	 1128.50},
+    20:	{'dim':	268435456,	'res':	0.1493	    ,'scale':	 564.25},
+    21:	{'dim':	536870912,	'res':	0.0746	    ,'scale':	 282.12},
+    22:	{'dim':	1073741824,	'res':	0.0373	    ,'scale':	 141.06},
+    23:	{'dim':	2147483648,	'res':	0.0187	    ,'scale':	 70.53}
+    }
+
+
+def clscreen():
+    print(chr(27)+'[2j')
+    print('\033c')
+    print('\x1bc')
+    return
 
 def num2base(n, b):
     if n == 0:
@@ -25,9 +62,9 @@ def num2base(n, b):
         n //= b
     return ''.join(digits[::-1])
 
-def coo2tiles(latitude, longitude):
+def coo2tiles(latitude, longitude, level):
 
-    aux = (2**LEVEL)
+    aux = (2**level)
     
     sinLatitude = math.sin(latitude * math.pi/180)
 
@@ -86,6 +123,11 @@ def getImgs(qList):
             in_memory_file = io.BytesIO(resp)
             im = Image.open(in_memory_file)
             imList[q[0]] = im
+
+            
+            print(f"Image {len(imList)}/{len(qList)} downloaded ({100*len(imList)/len(qList):.1f}%)")
+           
+            
             # Save with index since it's async
 
     async def main(qs):
@@ -93,22 +135,23 @@ def getImgs(qList):
             await asyncio.gather(*[get(q, session) for q in qs])
 
     start = time.time()
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy()) # For windows
     asyncio.run(main(qList))
     end = time.time()
 
-    print("\n"*20)
-    print(f"Took {end - start} seconds to pull {len(qList)} tiles.")
+    print(f"\nTook {end - start:.4f} seconds to download {len(qList)} tiles.")
+    print(f"{(end - start)/len(qList):.4f} seconds per tile.")
     return imList
 
 
-def main(lat, lon, kmX, kmY, filename="bing_map_pull"):
+def main(lat, lon, kmX, kmY, level, filename="bing_map_pull"):
 
 
-    cTileX, cTileY = coo2tiles(lat, lon)
+    cTileX, cTileY = coo2tiles(lat, lon, level)
 
-    kmPerPixel = 0.2986/1000
-    pixelsX  = kmX/kmPerPixel
-    pixelsY  = kmY/kmPerPixel
+    KMPERPIXEL = DATA[level]['res']/1000
+    pixelsX  = kmX/KMPERPIXEL
+    pixelsY  = kmY/KMPERPIXEL
 
     numTilesX = math.floor(pixelsX/256)
     numTilesY = math.floor(pixelsY/256)
@@ -143,6 +186,13 @@ def main(lat, lon, kmX, kmY, filename="bing_map_pull"):
     new_im.save(filename + ".jpg" )
     # new_im.show()
 
+    
+    print(f'\nGround Resolution (m/px): {DATA[level]["res"]}')
+    print(f'Map Width (px): {numTilesX*256}')
+    print(f'Map Height (px): {numTilesY*256}')
+    print(f'Map Scale (at 96 dpi):  1:{DATA[level]["scale"]}')
+    print(f'Zoom level: {LEVEL}')
+
     return 
 
 if __name__ == "__main__":
@@ -154,10 +204,11 @@ if __name__ == "__main__":
     parser.add_argument('-s', '--south', required=True, help="Km's to South")
     parser.add_argument('-x', '--lat', required=True, help="Initial latitude")
     parser.add_argument('-y', '--lon', required=True, help="Initial longitude")
+    parser.add_argument('-l', '--level', required=False, default=19, help="Zoom level (1-23). defaults to 19")
     parser.add_argument('-f', '--filename', required=False, help="Output filename (without extension)")
 
     args = parser.parse_args()
     
-    main(args.lat, args.lon, args.e, args.s, args.filename)
+    main(args.lat, args.lon, args.e, args.s, args.level, args.filename)
 
     
--- test.py
+++ test.py
@@ -2,7 +2,9 @@ from main import main
 
 lat = -38.203799 
 lon =  -69.388419 
-e = 5 
-s = 5
-main(lat, lon, e, s, "test")
+e = 1 
+s = 1
+level = 18
+
+main(lat, lon, e, s, level, "test")
 
